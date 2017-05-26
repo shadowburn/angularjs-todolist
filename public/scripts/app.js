80e811c216dc164c28b10aff1f7b6c95ef912d41
Jizi Zhang <zjzottawa@gmail.com>
2017-05-26 01:28:00 -0400

    Created API controller

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,3 @@
-angular.module("todoListApp", [])
+'use strict';
 
-.controller('mainCtrl', function($scope) {
-  $scope.helloWorld = function() {
-    console.log("Hello there!");
-  };
-
-  dataService.getTodos(function(response) {
-    console.log(response.data);
-    $scope.todos = response.data;
-  });
-
-  $scope.deleteTodo = function(todo, $index) {
-    dataService.deleteTodo(todo);
-    $scope.todos.splice($index, 1);
-  }
-
-})
-.service('dataService', function($http) {
-  this.getTodos = function(callback) {
-    $http.get('mock/todos.json').then(callback)
-  };
-
-  this.deleteTodo = function(todo){
-    console.log(todo.name + "has been deleted!");
-  };
-
-  this.saveTodos = function(todo) {
-    console.log(todo.name + "has been saved!")
-  };
-});
+angular.module('todoListApp', []);
